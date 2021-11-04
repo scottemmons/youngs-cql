910bb8515689cc3765d0839ab506943a49ee0667
Scott Emmons <scott@scottemmons.com>
2021-11-03 17:07:55 -0700

    Launch cql-antmaze on cpu

--- slurm/sbatch.sh
+++ slurm/sbatch.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=cql-antmaze
 #SBATCH --open-mode=append
-#SBATCH --output=logs/out/%x_%j.txt
-#SBATCH --error=logs/err/%x_%j.txt
-#SBATCH --time=20:00:00
-#SBATCH --mem=16G
+#SBATCH --output=logs/out/%A_%a.txt
+#SBATCH --error=logs/err/%A_%a.txt
+#SBATCH --time=23:59:59
+#SBATCH --mem-per-cpu=2G
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:GTX2080TI:1
 #SBATCH --account=co_rail
-#SBATCH --partition=savio3_gpu
-#SBATCH --qos=savio_lowprio
+#SBATCH --partition=savio3
 
 arrENVS=(${ENVS//;/ })
 
@@ -20,8 +18,8 @@ singularity exec --nv -B /usr/lib64 -B /var/lib/dcv-gl --overlay $SCRATCH/singul
 
 source /ext3/env.sh
 conda activate SimpleSAC
+cd $SCRATCH/youngs-cql
 export PYTHONPATH="$PYTHONPATH:$(pwd)"
 
-cd $SCRATCH/youngs-cql
-python -m SimpleSAC.conservative_sac_main --eval_period 100 --eval_n_trajs 100 --policy_lr 1e-4 --cql_target_action_gap 5.0 --cql_min_q_weight 5.0 --env '${arrENVS[$ENV_ID]}' --seed=${SEED_ID} --output_dir './experiment_output' --device='cuda'
+python -m SimpleSAC.conservative_sac_main --eval_period 100 --eval_n_trajs 100 --cql.policy_lr 1e-4 --cql.cql_target_action_gap 5.0 --cql.cql_min_q_weight 5.0 --env '${arrENVS[$ENV_ID]}' --seed=${SEED_ID} --logging.output_dir './experiment_output' --device='cpu' --logging.project 'cql' --logging.online
 "
